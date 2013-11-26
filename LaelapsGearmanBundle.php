fb3a5bf4107f95d6bb579d00df857c6b6619f586
Craig Blanchette <craig.blanchette@gmail.com>
2013-11-26 12:19:08 -0500

    Rename Bundle so it has proper namespace "laelaps_gearman" instead of "gearman"

---
+++ LaelapsGearmanBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Laelaps\GearmanBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Mateusz Charytoniuk <mateusz.charytoniuk@absolvent.pl>
+ */
+class LaelapsGearmanBundle extends Bundle {}
